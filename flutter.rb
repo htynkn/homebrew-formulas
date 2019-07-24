59d40d3c87ace6e2faf7d95ae279ecf617cfb452
Huang YunKun <htynkn@gmail.com>
2019-07-24 19:15:19 +0800

    Update flutter.rb

--- flutter.rb
+++ flutter.rb
@@ -3,7 +3,7 @@ class Flutter < Formula
   homepage "https://flutter.io/"
   version "v1.7.8+hotfix.3"
   url "https://storage.flutter-io.cn/flutter_infra/releases/stable/macos/flutter_macos_v1.7.8+hotfix.3-stable.zip"
-  sha256 "24703764088b5e89cd8f7e155a8178fc8c3e0cb0d8906667798fe0d41279b7a6"
+  sha256 "1bd5d165c79efebfba39e4602d6e07e74b40798630eb7173caedf0f664c53035"
   
   bottle :unneeded
 
