412b2e814770d2ce95118ba6addef6b40712c87e
Huang YunKun <htynkn@gmail.com>
2019-07-24 19:04:43 +0800

    Update flutter.rb

--- flutter.rb
+++ flutter.rb
@@ -1,6 +1,7 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
+  version "v1.7.8+hotfix.3"
   url "https://storage.flutter-io.cn/flutter_infra/releases/stable/macos/flutter_macos_v1.7.8+hotfix.3-stable.zip"
   sha256 "24703764088b5e89cd8f7e155a8178fc8c3e0cb0d8906667798fe0d41279b7a6"
   
